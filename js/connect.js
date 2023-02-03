da0973b14efd71adaae85298990215ee9d86d32e
Chaplin16 <chaplin16@orange.fr>
2023-02-03 15:24:18 +0100

    test fr btn username

--- js/connect.js
+++ js/connect.js
@@ -2,12 +2,20 @@ import { auth } from "../backend/firebase/auth.js";
 import { signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 let btnSubmit = document.getElementById("btnSubmit");
 
+let connect = document.getElementById("connect");
+
+connect.innerHTML += `<ul>
+               <a href="connect.html" class="buttonConnect" id="user">Bonjour ${info.username}</a>
+               <li>
+                  <a href="#" id="disconnect">Se déconnecter</a>
+               </li>
+            </ul>`
+
+
+
+
 btnSubmit.addEventListener("click", function (event) {
   let formLogin = document.getElementById("formLogin");
-  let connect = document.getElementById("connect");
-  let linkTop = document.getElementById("linkTop");
-  let linkTop1 = document.getElementById("linkTop1");
-
   event.preventDefault();
 
   if (formLogin.reportValidity() == true) {
@@ -23,12 +31,11 @@ btnSubmit.addEventListener("click", function (event) {
     signInWithEmailAndPassword(auth,contact.email, contact.password)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log(user)
-        alert("Vous êtes connecté 👍")
+        const info = JSON.parse(sessionStorage.getItem("username"));
         sessionStorage.setItem("user", JSON.stringify(user));  
-        sessionStorage.setItem("name",JSON.stringify(contact.username));  
-        window.location.assign("index.html")
- 
+        sessionStorage.setItem("username",JSON.stringify(contact.username));  
+        window.location.assign("index.html")      
+
       })
       .catch((error) => {
         const errorCode = error.code;
