40162fd963b59e63a4b4c330a0a4a10cb9e57613
Chaplin16 <chaplin16@orange.fr>
2023-02-12 14:10:26 +0100

    gchange file animal.js

--- js/carousel-index.js
+++ js/carousel-index.js
@@ -1,3 +1,57 @@
+const animals = [
+    {        
+        id:1,
+        attachment:"../images/chat.jpg",
+        alt:"très beau chat"
+    },
+    {        
+        id:2,
+        attachment:"../images/renard-polaire.jpg",
+        alt:"renard polaire magnifique"
+    },
+    {        
+        id:3,
+        attachment:"../images/tortue.jpg",
+        alt:"tortue de mer"
+    },
+    {        
+        id:4,
+        attachment:"../images/cygne.jpg",
+        alt:"cygne magnifique"
+    },
+    {        
+        id:5,
+        attachment:"../images/panda.jpg",
+        alt:"panda gourmand"
+    },
+    {        
+        id:6,
+        attachment:"../images/serpent.jpg",
+        alt:"serpent terrifiant"
+    },
+    {        
+        id:7,
+        attachment:"../images/oie.jpg",
+        alt:"oie en colère"
+    },
+    {        
+        id:8,
+        attachment:"../images/mouette.jpg",
+        alt:"mouette du grau du roi"
+    },
+    {        
+        id:9,
+        attachment:"../images/hamster.jpg",
+        alt:"hamster mignon"
+    },
+    {        
+        id:10,
+        attachment:"../images/renard-roux.jpg",
+        alt:"renard des bois"
+    }
+];
+
+
 function carousel(selector, data) {
     let element = document.querySelector(selector);
     data.forEach(function(d) {
@@ -9,7 +63,7 @@ function carousel(selector, data) {
     $(element).slick({
         centerMode: false,
         centerPadding: '40px',
-        slidesToShow: 4,
+        slidesToShow: 5,
         adaptiveHeight: true,
         speed:1000,
         dots:true,
@@ -22,28 +76,33 @@ function carousel(selector, data) {
                 breakpoint: 1650,
                 settings: {
                     centerPadding: '20px',
-                    slidesToShow: 3,
-                    dots:true,
+                    slidesToShow: 4,
                 }
             },
             {
                 breakpoint: 1250,
                 settings: {
                     centerPadding: '40px',
-                    slidesToShow: 2,
-                    dots:true,
+                    slidesToShow: 3,
                 }
             },
             {
                 breakpoint: 850,
                 settings: {
                     centerPadding: '40px',
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    centerPadding: '40px',
                     slidesToShow: 1,
-                    dots:true,
                 }
             }
         ]
     });
 }
 
-carousel(".carousel-slick", animals);
+carousel(".carousel", animals);
+
